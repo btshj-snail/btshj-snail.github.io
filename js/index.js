ea33379935dfe8614568fc0bf4a1480ca10d4a58
snail <btshjhewei@163.com>
2017-07-05 23:36:52 +0800

    '去掉相关样式'

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
 /**
  * Created by snail on 2017/7/5.
  */
-
-
-$(function(){
+function whenImageComplete(){
     snailUtils.loadImagesComplete(function(){
         snailUtils.endLoading();
     })
+}
+
+function typesEvent(){
+    $(".types").find("li")
+        .mouseover(function () {
+            $(this).find("a").hide();
+            $(this).find("span").show();
+        })
+        .mouseout(function () {
+            $(this).find("a").show();
+            $(this).find("span").hide();
+        })
+        .click(function () {
+           let url =  $(this).attr("pageUrl");
+           window.location.href = url;
+        })
+
+}
+
+$(function(){
+    whenImageComplete();
+    typesEvent();
 })
