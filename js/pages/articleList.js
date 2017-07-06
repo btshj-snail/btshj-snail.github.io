9d69cefb919a8ae03599679b7be28883a008f70d
btshjhewei@163.com <btshjhewei@163.com>
2017-07-06 18:06:58 +0800

    'hah'

--- js/pages/articleList.js
+++ js/pages/articleList.js
@@ -1,3 +1,12 @@
 /**
  * Created by snail on 2017/7/5.
  */
+function whenImageComplete(){
+    snailUtils.loadImagesComplete(function(){
+        snailUtils.endLoading();
+    })
+}
+
+$(function(){
+    whenImageComplete();
+})
