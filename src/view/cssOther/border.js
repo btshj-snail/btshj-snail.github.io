817bfd800a41396c7cc175ba278ba50c13edfda1
snail <btshjhewei@163.com>
2017-07-27 22:59:56 +0800

    'update'

--- src/view/cssOther/border.js
+++ src/view/cssOther/border.js
@@ -7,24 +7,12 @@ import snailUtils from '../../publicResource/libs/snailUtils';
 
 
 const pageAnimate  = {
-    doHeadSectionAnimate(){
-        let $o = $("#headSectionAnimate");
-        let $span = $o.find("span").hide();
-        $o.addClass("animate_text_center");
-        setTimeout(function () {
-            $span.show();
-            $o.addClass("animate_text_second");
-            setTimeout(function () {
-                $o.addClass("animate_text_finally");
-            }, 1600)
-        }, 1600)
-    }
+
 }
 
 
 $(function(){
     snailUtils.loadImagesComplete(function () {
         snailUtils.endLoading();
-        pageAnimate.doHeadSectionAnimate();
     });
 })
